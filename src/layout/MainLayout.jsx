56213a73f44f6971ddd6e0fb3e4c1397141ac807
Accoder777 <rozimurodjumanazarovv@gmail.com>
2024-11-15 10:02:10 +0500

    homework done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Header from "../components/header/Header";
 import { Outlet } from "react-router-dom";
+import { CreatedContext } from "../pages/context/UserContext";
 
 const registered = [
   {
@@ -43,31 +44,22 @@ const registered = [
   },
   {
     href: "/dashboard",
-    title: "Suggest me",
+    title: "",
     icon: (
       <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-user"
       >
-        <path
-          d="M9.61987 3.95331L13.6665 7.99997L9.61987 12.0466"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeMiterlimit="10"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M2.33325 8H13.5533"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeMiterlimit="10"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
+        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+        <circle cx="12" cy="7" r="4"></circle>
       </svg>
     ),
   },
@@ -115,7 +107,7 @@ const registration = [
   {
     href: "/login",
     title: "",
-    icon: 
+    icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -126,19 +118,31 @@ const registration = [
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        class="feather feather-log-in"
+        className="feather feather-log-in"
       >
         <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
         <polyline points="10 17 15 12 10 7"></polyline>
         <line x1="15" y1="12" x2="3" y2="12"></line>
       </svg>
+    ),
   },
 ];
 
 const MainLayout = () => {
+  const [navListData, setNavListData] = useState([]);
+  const { sessionId, accountId } = useContext(CreatedContext);
+
+  useEffect(() => {
+    if (!sessionId || !accountId) {
+      setNavListData(registration);
+    } else {
+      setNavListData(registered);
+    }
+  }, [sessionId, accountId]);
+
   return (
     <>
-      <Header navList={registration} />
+      <Header navList={navListData} />
       <div className="outlet">
         <Outlet />
       </div>
